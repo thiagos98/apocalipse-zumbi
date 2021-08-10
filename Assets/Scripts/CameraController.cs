1ce611413c92dabf654df8de2746e00535a74e7a
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-08-10 16:10:23 -0400

    Adicionar animação jogador,movimentar camera,adicionar colisão,adicionar física
    - Adicionar animação.
    - Movimentar câmera.
    - Adicionar colisão.
    - Adicionar física.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] GameObject m_Player;
+    private Vector3 m_DistCompensar;
+
+	void Start () {
+        m_DistCompensar = transform.position - m_Player.transform.position;
+	}
+	
+	void Update () {
+        transform.position = m_Player.transform.position + m_DistCompensar;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] float m_Speed;
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update ()
+	{
+		var axisX = Input.GetAxis("Horizontal");
+		var axisZ = Input.GetAxis("Vertical");
+		
+		direction = new Vector3(axisX, 0, axisZ);
+
+        m_Rigidbody.MovePosition
+            (m_Rigidbody.position + 
+            (direction * (Time.deltaTime * m_Speed)));
+
+        if(direction != Vector3.zero)
+        {
+            m_Animator.SetBool("isMove", true);
+        }
+        else
+        {
+            m_Animator.SetBool("isMove", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rigidbody.MovePosition
+            (m_Rigidbody.position +
+            (direction * (Time.deltaTime * m_Speed)));
+    }
+}
