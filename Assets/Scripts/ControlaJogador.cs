2e85e4170fec8cad78d009544109afc6fbffb6bc
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-08-10 11:55:33 -0400

    Criar cenário,adicionar textura/material
    - Criar cenário.
    - Adicionar textura/material.
    - Adicionar player.
    - Adicionar movimentação do player.

---
+++ Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+	[SerializeField] float m_Speed;
+	private void Update ()
+	{
+		var axisX = Input.GetAxis("Horizontal");
+		var axisZ = Input.GetAxis("Vertical");
+		
+		var direction = new Vector3(axisX, 0, axisZ);
+		transform.Translate(direction * (Time.deltaTime * m_Speed));
+	}
+}
