ea8b9bfe26e285e4d18195b1a9f059afd0679eaf
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-08-20 23:29:57 -0400

    Adicionar chefe, adicionar nav mesh para o chefe.
    - Adicionar chefe.
    - Adicionar navmesh para o chefe.

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : MonoBehaviour
+{
+    private Transform _player;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _player = GameObject.FindWithTag(Tags.Player).transform;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(_player.position);
+    }
+}
