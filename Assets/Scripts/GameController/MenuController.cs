1de5ac780e556937f358bad32fe7d356de0ab1ab
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-08-25 12:54:11 -0400

    Adicionar menu do jogo, adicionar efeitos sonoros aos botões.
    - Adicionar menu do jogo.
    - Adicionar efeitos sonoros aos botões.

---
+++ Assets/Scripts/GameController/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject exitButton;
+
+    private void Start()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        exitButton.SetActive(true);
+#endif
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(ChangeScene(Scenes.Game));
+    }
+
+    private IEnumerator ChangeScene(string scene)
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        StartCoroutine(Exit());
+    }
+
+    private IEnumerator Exit()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
---
+++ Assets/Scripts/GameController/Scenes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenes : MonoBehaviour
+{
+    public const string Game = "Game";
+}
